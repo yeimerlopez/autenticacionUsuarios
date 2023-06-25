d2dbd03e46299db31aae361c742a414b47586fb1
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-25 17:04:03 -0500

    proyecto funcionando hasta la captura de user y pass lo veo en la consola 16:06

--- src/app.py
+++ src/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request, redirect, url_for
 from config import config
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('auth/login.html')
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['username'])
+        print(request.form['password'])
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
