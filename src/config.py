8fdd2020f760dad444a10ffda068422861ead685
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-25 23:34:19 -0500

    proyecto funcional hasta la creacion de password_hash 22:00

--- src/config.py
+++ src/config.py
@@ -1,5 +1,13 @@
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
+
+
+class DevelopmentConfig(Config):
     DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '1234'
+    MYSQL_DB = 'flak_login'
 
 
 config={
