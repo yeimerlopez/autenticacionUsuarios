66d1c43f23961f68715bb1f1b48916c41e296347
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-26 00:33:00 -0500

    error en prueba30:17

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template,request, redirect, url_for
+from flask import Flask, render_template,request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
 from config import config
+#models
+from models.ModelUser import ModelUser
+
+#Entities
+from models.entities.User import User
+
 
 app=Flask(__name__)
+
+db=MySQL(app)
 
 @app.route('/')
 def index():
@@ -10,11 +20,29 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        print(request.form['username'])
-        print(request.form['password'])
+        # print(request.form['username'])
+        # print(request.form['password'])
+        user = User(0,request.form['username',request.form['password']])
+        logged_user=ModelUser.login(db,user)
+        if logged_user != None:
+            if logged_user.password:
+                return redirect(url_for('home'))
+            else:
+                flash("Contraseña invalida!!")
+                return render_template('auth/login.html')
+
+        else:
+            flash("Usuario no encontrado")
+            return render_template('auth/login.html')
+
+
         return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,4 +1,4 @@
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
 
 class User():
@@ -13,4 +13,4 @@ class User():
     def check_password(self, hashed_password,password):
         return check_password_hash(hashed_password,password)
     
-    print(generate_password_hash("hola"))
+    
